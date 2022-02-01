8200b2178bbf584aa1894b0b3203100d0b32b6bd
muneza <muneza@gmail.com>
2022-02-01 21:13:51 +0300

    new commit

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import Slider from "react-slick";
-// import SliderInfo from './ImageSlide__'
 import yoga from "./images/fleece.jpg";
 import fleece from "./images/fl.jpg";
 import yk from "./images/hoodie.jpg";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import yao from "./images/yoga.jpg";
@@ -14,9 +12,10 @@ import { Link } from "@material-ui/core";
 export default function Carousel() {
   const settings = {
     dots: true,
-    autoplay: true,
+    autoplay: false,
     infinite: true,
     speed: 500,
+    arrows: false,
     slidesToShow: 3,
     slidesToScroll: 1
   };
@@ -28,44 +27,25 @@ export default function Carousel() {
   return (
     <div>
 
-      <Slider {...settings}>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
+      <Slider className="wuzi_slider" {...settings}>
+        <div className="wuzi_slide">
+        <h1 className="carousel__price">$100</h1>
          <img className="carousel__image" src={yao} />
         </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
-         <img className="carousel__image" src={yoga} />
-        </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
+       
+        <div className="wuzi_slide">
+        <h1 className="carousel__price">$100</h1>
          <img className="carousel__image" src={yk} />
         </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
+        <div className="wuzi_slide">
+        <h1 className="carousel__price">$100</h1>
          <img className="carousel__image" src={yao} />
         </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
+        <div className="wuzi_slide">
+        <h1 className="carousel__price">$100</h1>
          <img className="carousel__image" src={yk} />
         </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
-         <img className="carousel__image" src={yk} />
-        </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
-         <img className="carousel__image" src={yk} />
-        </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
-         <img className="carousel__image" src={yk} />
-        </div>
-        <div>
-        <h1 className="carousel__price">UGX 100,000</h1>
-         <img className="carousel__image" src={yk} />
-        </div>
-      </Slider>
+              </Slider>
     </div>
   );
 }
