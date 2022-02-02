9b56ac0a86e58cf54a6e3eeea1b7fea0466382a9
muneza <muneza@gmail.com>
2022-02-02 21:54:13 +0300

    5 days to and m still lagging

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,50 +2,55 @@ import React, { useState } from "react";
 import "./Carousel.css";
 import Slider from "react-slick";
 import yoga from "./images/fleece.jpg";
-import fleece from "./images/fl.jpg";
-import yk from "./images/hoodie.jpg";
+import fleece from "./images/dummy.jpg";
+import yk from "./images/dummy.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import yao from "./images/yoga.jpg";
+import yao from "./images/dummy.jpg";
 import { Link } from "@material-ui/core";
 
 export default function Carousel() {
   const settings = {
     dots: true,
-    autoplay: false,
+    autoplay: true,
     infinite: true,
-    speed: 500,
+    speed: 400,
     arrows: false,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
   const styles = {
     backgroundImage: `url(${fleece})`,
   };
 
- 
   return (
-    <div>
-
+    <div className="wuzi_slider_slide">
       <Slider className="wuzi_slider" {...settings}>
         <div className="wuzi_slide">
-        <h1 className="carousel__price">$100</h1>
-         <img className="carousel__image" src={yao} />
-        </div>
-       
-        <div className="wuzi_slide">
-        <h1 className="carousel__price">$100</h1>
-         <img className="carousel__image" src={yk} />
+          <h1 className="carousel__price">$100</h1>
+          <img className="carousel__image" src={yk} />
         </div>
         <div className="wuzi_slide">
-        <h1 className="carousel__price">$100</h1>
-         <img className="carousel__image" src={yao} />
+          <h1 className="carousel__price">$100</h1>
+          <img className="carousel__image" src={yao} />
         </div>
         <div className="wuzi_slide">
-        <h1 className="carousel__price">$100</h1>
-         <img className="carousel__image" src={yk} />
+          <h1 className="carousel__price">$100</h1>
+          <img className="carousel__image" src={yk} />
         </div>
-              </Slider>
+        <div className="wuzi_slide">
+          <h1 className="carousel__price">$100</h1>
+          <img className="carousel__image" src={yk} />
+        </div>
+        <div className="wuzi_slide">
+          <h1 className="carousel__price">$100</h1>
+          <img className="carousel__image" src={yao} />
+        </div>
+        <div className="wuzi_slide">
+          <h1 className="carousel__price">$100</h1>
+          <img className="carousel__image" src={yk} />
+        </div>
+      </Slider>
     </div>
   );
 }
--- src/CustomComponents/StillIntrested.js
+++ src/CustomComponents/StillIntrested.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Carousel from "../Carousel"
+import "./StillIntrested.css";
 
 function StillIntrested() {
     return (
         <div>
             <div>
-                <h2>Still Intrested ?</h2>
+                <h2  className="still-intrested-heading">Still Intrested ?</h2>
             </div>
             <Carousel/>
             
